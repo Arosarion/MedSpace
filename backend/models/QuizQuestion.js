174875d95afa4a85d57c0a691cbff6673a53ebc3
Diego Ragosta <di154020@ucf.edu>
2026-06-24 12:44:14 -0400

    Add QuizQuestion model with schema definition

--- backend/models/QuizQuestion.js
+++ backend/models/QuizQuestion.js
@@ -1,1 +1,30 @@
+//models/QuizQuestion
 
+const mongoose = require("mongoose ");
+
+const quizQuestionSchema = new mongoose.Schema(
+    {
+        topicSlug: {
+            type: String,
+            required: true
+        },
+        question: {
+            type: String,
+            required: true
+        },
+        choices: {
+            type: [String],
+            required: true
+        },
+        correctAnswer: {
+            type: String,
+            required: true
+        },
+        explanation: {
+            type: String,
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("QuizQuestion", quizQuestionSchema);
