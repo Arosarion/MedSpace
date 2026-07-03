3d5c24eadfdd988c509ab4345575794cb0e70e10
Matthew <ma016465@ucf.edu>
2026-07-03 19:33:20 +0000

    User and Score mongoose schemas

--- backend/models/Score.js
+++ backend/models/Score.js
@@ -1,1 +1,27 @@
+const mongoose = require("mongoose");
 
+const scoreSchema = new mongoose.Schema({
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  questionsAnswered: {
+    type: Number,
+    required: true
+  },
+  correctCount: {
+    type: Number,
+    required: true
+  },
+  timeTakenSeconds: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Score", scoreSchema);
