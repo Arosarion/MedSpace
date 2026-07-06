80258e3ec604323691621e1b6b6c566285b6482d
Diego Ragosta <di154020@ucf.edu>
2026-07-06 13:17:52 -0400

    Update imports to use quizController

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {
     getQuestionsByTopic,
     createQuestion
-} = require("../controllers/questionController");
+} = require("../controllers/quizController");
 
 router.get("/:topicSlug", getQuestionsByTopic);
 router.post("/", createQuestion);
